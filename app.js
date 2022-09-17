852c15aa282df0d325640cf0523287e0ec36e26f
MarioPacaneaSeptari <89852955+MarioPacaneaSeptari@users.noreply.github.com>
2022-09-17 15:11:59 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+function barcode() {
+
+    var display = document.getElementById("full_top")
+    display.style.visibility = "hidden";
+  
+    var reveal = document.getElementById("tap")
+    reveal.innerHTML = "Tap to view color bar";
+  
+  
+  
+  
+  }
+  function clock(){
+    const fullDate = new Date();
+    var hours = fullDate.getHours();
+    var minutes = fullDate.getMinutes();
+    var seconds = fullDate.getSeconds();
+  
+  if (hours<10){
+    hours = "0" + hours
+  }
+  if (minutes<10){
+    minutes = "0" + minutes
+  }
+  if (seconds<10){
+    seconds = "0" + seconds
+  }
+  
+  
+  document.getElementById('hours').innerHTML = hours;
+  document.getElementById('minutes').innerHTML = ":" + minutes;
+  document.getElementById('seconds').innerHTML = ":" + seconds;
+  }
+  
+  setInterval(clock,100);
